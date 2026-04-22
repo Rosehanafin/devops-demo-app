c665096272c68822eb9319c0e8b098a1ab88bb26
Rose Hanafin <rosehanafin@roses-air-3.bc.edu>
2026-04-22 13:06:36 -0400

    Add real test and complete v2.0.0 pipeline

--- tests/server.test.js
+++ tests/server.test.js
@@ -1,1 +1,10 @@
-test('sample test', () => { expect(true).toBe(true); });
+const request = require('supertest');
+const app = require('../server');
+
+describe('GET /api/info', () => {
+  it('should return version 2.0.0', async () => {
+    const response = await request(app).get('/api/info');
+    expect(response.statusCode).toBe(200);
+    expect(response.body.version).toBe('2.0.0');
+  });
+});
